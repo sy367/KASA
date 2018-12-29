9f573826beb79e734534c71fe4b163b8d85ed30d
Seokjoo Yoon <sy367@cornell.edu>
2018-12-29 18:32:11 -0500

    component for alumni page

---
+++ my-app/src/components/Alumni.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Alumni extends Component {
+  render() {
+    return (
+      <div id="gajok-container">
+        <h1>Alumni</h1>
+      </div>
+    );
+  }
+}
