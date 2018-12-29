1d9b0f57451cdee1abc363518a71e0ce47bfbcbd
Seokjoo Yoon <sy367@cornell.edu>
2018-12-29 18:57:49 -0500

    faqs page component

---
+++ my-app/src/components/FAQs.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export default class FAQs extends Component {
+  render() {
+    return (
+
+      <div id="gajok-container">
+        <h1>FAQs</h1>
+      </div>
+
+    );
+  }
+}
