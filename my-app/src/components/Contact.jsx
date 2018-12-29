2dc492bf53fed132f15ea9fca36da38069634e19
Seokjoo Yoon <sy367@cornell.edu>
2018-12-29 18:57:26 -0500

    contact form component

---
+++ my-app/src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Contact extends Component {
+  render() {
+    return (
+      <div id="gajok-container">
+        <h1>Contact KASA</h1>
+      </div>
+    );
+  }
+}
