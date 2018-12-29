6d56b0b589dbe94dd293dad6be719155334fa6ac
Seokjoo Yoon <sy367@cornell.edu>
2018-12-29 18:58:10 -0500

    gajok page component

---
+++ my-app/src/components/Gajok.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import '../styles/gajok.css';
+
+export default class Gajok extends Component {
+  render() {
+    return (
+      <div id="gajok-container">
+        <h1>Gajok</h1>
+      </div>
+    );
+  }
+}
