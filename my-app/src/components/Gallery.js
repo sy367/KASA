70e400191a96d595bed0104f4d68e23f2e65f4cc
Seokjoo Yoon <sy367@cornell.edu>
2019-01-23 00:53:03 -0500

    added gallery page in place of alumni

---
+++ my-app/src/components/Gallery.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Footer from './Footer.jsx';
+
+export default class Gallery extends Component {
+  render() {
+    return (
+      <div id="gajok-container">
+        <h1>Gallery</h1>
+
+        
+
+        <Footer />
+      </div>
+    );
+  }
+}
